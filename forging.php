ddf169ad20695da5dc04a147d7fd3005560dd522
dgoldman0 <admin@danielgoldman.us>
2020-04-22 13:55:31 -0400

    Update forging.php

--- forging.php
+++ forging.php
@@ -9,18 +9,19 @@
 
 	  gtag('config', 'UA-114409253-4');
 	</script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="/js/tronweb.js"></script>
-    <script src="/js/abi.js"></script>
-    <script src="/js/forging.js"></script>
-    <link rel="stylesheet" href="/css/style.css">
-    <title>World Builder Forging Platform</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="/js/tronweb.js"></script>
+  <script src="/js/abi.js"></script>
+  <script src="/js/forging.js"></script>
+  <link rel="stylesheet" href="/css/style.css">
+  <title>World Builder Forging Platform</title>
 	<meta property="og:title" content="World Builder Project" />
 	<meta property="og:description" content="World Builder's ecosystem runs on a number of different tokens associated with different projects. The forging platform allows users to forge new tokens using WRLD as base material." />
-	<meta property="og:url" content="https://worldbuilder.fun" />
+	<meta property="og:url" content="https://wrldcoin.io" />
 	<meta property="og:locale" content="en_US" />
 	<meta property="og:type" content="website" />
 	<meta property="og:site" content="World Builder" />
@@ -39,7 +40,7 @@
 	</style>
   </head>
   <body>
-    <?php require_once('/inc/menu_bar.php'); ?>
+    <!--<?php require_once('/inc/menu_bar.php'); ?>-->
     <div id = "main" style = "margin-top:10px;">
 		<h1><center>Welcome to The Forge</center></h1>
 		<div class = "row" style = "margin-bottom: 2px;" id = "information-panel">
