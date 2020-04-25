999af3cb0575e7f04c2fca55e951bbd8da94e6b2
dgoldman0 <admin@danielgoldman.us>
2020-04-25 11:01:59 -0400

    Update menu_bar.php

--- inc/menu_bar.php
+++ inc/menu_bar.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/">Home (Game List)<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="https://blog.worldbuilder.fun">Blog<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="https://worldbuilder.fun">Mini Games<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="https://t.me/tworldbuilder">Telegram<span class="sr-only">(current)</span></a>
@@ -14,9 +14,6 @@
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="https://tronscan.org/#/token/1002567">WRLD<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="/faucet.php">Faucet<span class="sr-only">(current)</span></a>
       </li>
     </ul>
 </nav>
