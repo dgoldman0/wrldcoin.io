7ede62d723d46ab6cc6ccc5aefaf1b077e5f6fcc
dgoldman0 <45105852+dgoldman0@users.noreply.github.com>
2021-12-18 09:09:04 -0500

    Create rai_odette.php

---
+++ charity/rai_odette.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Charity Support | World Builder</title>
+
+        <!-- CHARSET AND VIEWPORT METAS AT THE TOP, BELOW TITLE ALWAYS. -->
+        <meta charset="utf8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+
+        <!-- NOW SOME DESCRIPTION METAS -->
+        <meta name="description" content="World Builder Charity Support - OdettePH Relief">
+        <meta name="keywords" content="cryptocurrency, crypto, blockchain, money, finance">
+
+        <!-- CSS, ICONS AND BOOTSTRAP -->
+        <link rel="stylesheet" href="https://bootswatch.com/4/pulse/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/world.css" />
+        <link rel="icon" type="image/png" href="assets/img/etc.png">
+
+        <!-- FontAwesome -->
+        <script src="https://kit.fontawesome.com/f352960784.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body class="wallpaper">
+        <?php include('modules/navbar.php'); ?>
+
+        <!-- SPACING OF 5 BR'S NECESSARY FOR TOP OF CONTENT TO SHOW WITH STICKY NAVBAR. -->
+        <br /><br /><br /><br /><br />
+
+        <!-- MAIN CONTENT DIV -->
+        <div class="container">
+            <div class="row jumbotron glassPanel rcAll">
+                <div class="container">
+                    <div class="row">
+                        <div class="col text-left">
+                            <h1 class="text-white">Charity Support: Super Typhoon Rai/Odette Recovery</h1>
+                            <p class="text-success">Supporting cleanup efforts for those in the Philippines affected by Super Typhoon Rai/Odette.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card glassPanel border-light rcAll">
+                        <div class="card-header glassPanel text-white rcTop">Background</div>
+                        <div class="card-body">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col text-left">
+                                        <p class="text-white">
+                                          Super Typhoon Rai, locally named Odette, hit the Philippines on December 16, 2021, shortly after being upgraded to category 5 strength. This storm was one of the most powerful storms this year, and has caused significant damage to multiple regions in the Philippines. Many people had to evacuate. The storm caused significant property damage, and loss of life. Damage to local infrastructure, including the power grid, have made cleanup efforst difficult. To support the effort, World Builder is announcing its first charity support drive, utilizing its branch project, Promote Health and the Forge System.                                    
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer glassPanel rcBottom"></div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="card glassPanel border-light rcAll">
+                        <div class="card-header glassPanel text-white rcTop">Objective</div>
+                        <div class="card-body">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col text-center">
+                                      We will donate $100 per 1 million $PHC minted, up to $5,000 to Philippines charities supporting the recovery from Super Typhoon Rai/Odette. The marked supply is 25,354,494 $PHC. No minting will be counted for donations past 11:59PM EST on 12/26/21.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Project Dependencies -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/tronweb.js"></script>
+
+        <!-- Dapp Scripts -->
+        <script src="assets/lib/abi.js"></script>
+        <script src="assets/lib/forging.js"></script>
+    </body>
+</html>
