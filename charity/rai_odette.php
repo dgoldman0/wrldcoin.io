1da73527dc9c260f9fc50db9db14338526152c85
dgoldman0 <45105852+dgoldman0@users.noreply.github.com>
2021-12-18 10:01:38 -0500

    Update rai_odette.php

--- charity/rai_odette.php
+++ charity/rai_odette.php
@@ -22,7 +22,7 @@
         <meta property = "og:type" content = "website" />
         <meta property = "og:image" content = "https://wrldcoin.io/assets/img/logo.png" />
 
-        <meta name="twitter:card" content="Support those affected by OdettePH" />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@wrldcoin" />
         <meta name="twitter:title" content="World Buidler Charity Support - OdettePH Relief" />
         <meta name="twitter:description" content="Help World Builder support those affected by Super Typhoon Rai/Odette" />
