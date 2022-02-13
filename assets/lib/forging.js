ae131535032fe27f105d9ed92fb97a78beefbf7f
dgoldman0 <45105852+dgoldman0@users.noreply.github.com>
2022-02-13 11:56:12 -0500

    Update forging.js

--- assets/lib/forging.js
+++ assets/lib/forging.js
@@ -83,6 +83,7 @@ $(document).ready(function() {
 					$("#total-supply").text(Math.round(res / 1000000000000000000));
 				}
 			});
+			console.log(web3.eth.defaultAccount);
 			console.log("Address:" + web3.eth.getAccounts()[0]);
 			contract.methods.balanceOf(web3.eth.getAccounts()[0]).call(function (err, res) {
 				$('#balance').text(res.balance / 1000000000000000000);
